8b857b33851368dfad5e50564b33a5dcb688b350
Jayar Iglesias <jayariglesias@gmail.com>
2021-03-25 23:12:52 +0800

    up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import './App.css';
-
+// import './App.css';
+import styles from './App.css';
 function App() {
-  return (
-    <div className="App">
 
+  document.addEventListener('mousemove',(e) => {
+    let cursor = document.getElementsByClassName('cursor');
+
+    for(let i = 0; i < cursor.length; i++) {
+      cursor[i].style.left = e.clientX + 'px';
+      cursor[i].style.top = e.clientY + 'px';
+    }
+
+  })
+  
+  return (
+    <div className={styles.App}>
+        <div className="cursor"></div>
+        <div className="cursor"></div>
+        <div className="cursor"></div>
+        <div className="cursor"></div>
+        <div className="cursor"></div>
+        <div className="cursor"></div>
     </div>
   );
 }
+
 
 export default App;
