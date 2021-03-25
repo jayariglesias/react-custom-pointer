5fc70642f968c110df12ae05abb6cd2b5895c800
Jayar Iglesias <jayariglesias@gmail.com>
2021-03-26 03:58:30 +0800

    cursor

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from 'react'
-import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export default function Cursor(props) {
+
+  document.addEventListener('mousemove',(e) => {
+      document.getElementById('cursor').style.left = `${e.clientX}px`;
+      document.getElementById('cursor').style.top = `${e.clientY}px`;
+  });
+
+  document.body.style.cursor = props.type || 'default';
+
+  return (
+      <div id="cursor" style={{position:'absolute'}} className={props.className}>
+          {props.children}
+      </div>
+  )
 }
