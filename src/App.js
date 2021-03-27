efc39c749d55c3a667f106199d90536be47b5007
Jayar Iglesias <jayariglesias@gmail.com>
2021-03-27 12:31:58 +0800

    example

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import Cursor from './components/cursor';
+import Cursor from 'react-custom-pointer';
 import './styles/cursor.css';
-
 
 function App() {
   return (
     <>
-      <Cursor type="default">
+      <Cursor type="none">
         <div className="circle"></div>
         <div className="circle"></div>
       </Cursor>
+      <div>
+          <h1>Hello World!</h1>
+      </div>
     </>
   );
 }
-
 
 export default App;
